a9edca6aaab0a36c5de46140e0d6eff4183e77d0
Flavio Fonseca <flavio.fonseca@foursys.com.br>
2022-10-27 15:57:46 -0300

    Daily analytics OK

--- app/src/main/java/com/example/personalbudgetingapp/DailyAnalyticsActivity.java
+++ app/src/main/java/com/example/personalbudgetingapp/DailyAnalyticsActivity.java
@@ -35,6 +35,7 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.TimerTask;
 
 public class DailyAnalyticsActivity extends AppCompatActivity {
     private ActivityDailyAnalyticsBinding binding;
@@ -42,7 +43,7 @@ public class DailyAnalyticsActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private String onlineUserId = "";
     private ProgressDialog loader;
-    AnyChartView anyChartView = null;
+
 
 
     @Override
@@ -72,10 +73,17 @@ public class DailyAnalyticsActivity extends AppCompatActivity {
         getTotalWeekHealthExpenses();
         getTotalWeekPersonalExpenses();
         getTotalWeekOtherExpenses();
-
         getTotalDaySpending();
 
-        loadGraph();
+        new java.util.Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                loadGraph();
+                setStatusAndImageResource();
+            }
+        }, 200);
+
+
 
     }
 
@@ -572,7 +580,7 @@ public class DailyAnalyticsActivity extends AppCompatActivity {
                             .align(Align.CENTER);
 
 
-                    anyChartView.setChart(pie);
+                    binding.anyChartView.setChart(pie);
 
 
                 }else {
@@ -595,118 +603,289 @@ public class DailyAnalyticsActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()){
 
-                    int traTotal;
+                    //TODO - ver se precisa mudar os totais para float no loadGraph method ou para int aqui
+                    float traTotal;
                     if (snapshot.hasChild("dayTrans")){
                         traTotal = Integer.parseInt(snapshot.child("dayTrans").getValue().toString());
                     }else {
                         traTotal = 0;
                     }
 
-                    int foodTotal;
+                    float foodTotal;
                     if (snapshot.hasChild("dayFood")){
                         foodTotal = Integer.parseInt(snapshot.child("dayFood").getValue().toString());
                     }else {
                         foodTotal = 0;
                     }
 
-                    int houseTotal;
+                    float houseTotal;
                     if (snapshot.hasChild("dayHouse")){
                         houseTotal = Integer.parseInt(snapshot.child("dayHouse").getValue().toString());
                     }else {
                         houseTotal = 0;
                     }
 
-                    int entTotal;
+                    float entTotal;
                     if (snapshot.hasChild("dayEnt")){
                         entTotal = Integer.parseInt(snapshot.child("dayEnt").getValue().toString());
                     }else {
                         entTotal = 0;
                     }
 
-                    int eduTotal;
+                    float eduTotal;
                     if (snapshot.hasChild("dayEdu")){
                         eduTotal = Integer.parseInt(snapshot.child("dayEdu").getValue().toString());
                     }else {
                         eduTotal = 0;
                     }
 
-                    int chaTotal;
+                    float chaTotal;
                     if (snapshot.hasChild("dayCha")){
                         chaTotal = Integer.parseInt(snapshot.child("dayCha").getValue().toString());
                     }else {
                         chaTotal = 0;
                     }
 
-                    int appTotal;
+                    float appTotal;
                     if (snapshot.hasChild("dayApp")){
                         appTotal = Integer.parseInt(snapshot.child("dayApp").getValue().toString());
                     }else {
                         appTotal = 0;
                     }
 
-                    int heaTotal;
+                    float heaTotal;
                     if (snapshot.hasChild("dayHea")){
                         heaTotal = Integer.parseInt(snapshot.child("dayHea").getValue().toString());
                     }else {
                         heaTotal = 0;
                     }
 
-                    int perTotal;
+                    float perTotal;
                     if (snapshot.hasChild("dayPer")){
                         perTotal = Integer.parseInt(snapshot.child("dayPer").getValue().toString());
                     }else {
                         perTotal = 0;
                     }
 
-                    int othTotal;
+                    float othTotal;
                     if (snapshot.hasChild("dayOther")){
                         othTotal = Integer.parseInt(snapshot.child("dayOther").getValue().toString());
                     }else {
                         othTotal = 0;
                     }
 
-                    Pie pie = AnyChart.pie();
-                    List<DataEntry> data = new ArrayList<>();
-                    data.add(new ValueDataEntry("Transport", traTotal));
-                    data.add(new ValueDataEntry("House exp", houseTotal));
-                    data.add(new ValueDataEntry("Food", foodTotal));
-                    data.add(new ValueDataEntry("Entertainment", entTotal));
-                    data.add(new ValueDataEntry("Education", eduTotal));
-                    data.add(new ValueDataEntry("Charity", chaTotal));
-                    data.add(new ValueDataEntry("Apparel", appTotal));
-                    data.add(new ValueDataEntry("Health", heaTotal));
-                    data.add(new ValueDataEntry("Personal", perTotal));
-                    data.add(new ValueDataEntry("Other", othTotal));
-
-                    pie.data(data);
-                    pie.title("Daily Analytics");
-                    pie.labels().position("outside");
-                    pie.legend().title().enabled(true);
-                    pie.legend().title()
-                            .text("Itens Spent On")
-                            .padding(0d, 0d, 10d, 0d);
-                    pie.legend()
-                            .position("center-bottom")
-                            .itemsLayout(LegendLayout.HORIZONTAL)
-                            .align(Align.CENTER);
+                    float monthTotalSpentAmount;
+                    if (snapshot.hasChild("today")){
+                        monthTotalSpentAmount = Integer.parseInt(snapshot.child("today").getValue().toString());
+                    }else {
+                        monthTotalSpentAmount = 0;
+                    }
 
 
-                    anyChartView.setChart(pie);
+                    //Getting ratios
+
+                    float traRatio;
+                    if (snapshot.hasChild("dayTranspRatio")){
+                        traRatio = Integer.parseInt(snapshot.child("dayTranspRatio").getValue().toString());
+                    }else {
+                        traRatio = 0;
+                    }
+
+                    float foodRatio;
+                    if (snapshot.hasChild("dayFoodRatio")){
+                        foodRatio = Integer.parseInt(snapshot.child("dayFoodRatio").getValue().toString());
+                    }else {
+                        foodRatio = 0;
+                    }
+
+                    float houseRatio;
+                    if (snapshot.hasChild("dayHouseRatio")){
+                        houseRatio = Integer.parseInt(snapshot.child("dayHouseRatio").getValue().toString());
+                    }else {
+                        houseRatio = 0;
+                    }
+
+                    float entRatio;
+                    if (snapshot.hasChild("dayEntRatio")){
+                        entRatio = Integer.parseInt(snapshot.child("dayEntRatio").getValue().toString());
+                    }else {
+                        entRatio = 0;
+                    }
+
+                    float eduRatio;
+                    if (snapshot.hasChild("dayEducRatio")){
+                        eduRatio = Integer.parseInt(snapshot.child("dayEducRatio").getValue().toString());
+                    }else {
+                        eduRatio = 0;
+                    }
+
+                    float charRatio;
+                    if (snapshot.hasChild("dayCharRatio")){
+                        charRatio = Integer.parseInt(snapshot.child("dayCharRatio").getValue().toString());
+                    }else {
+                        charRatio = 0;
+                    }
+
+                    float apparelRatio;
+                    if (snapshot.hasChild("dayApparelRatio")){
+                        apparelRatio = Integer.parseInt(snapshot.child("dayApparelRatio").getValue().toString());
+                    }else {
+                        apparelRatio = 0;
+                    }
+
+                    float heaRatio;
+                    if (snapshot.hasChild("dayHealthRatio")){
+                        heaRatio = Integer.parseInt(snapshot.child("dayHealthRatio").getValue().toString());
+                    }else {
+                        heaRatio = 0;
+                    }
+
+                    float perRatio;
+                    if (snapshot.hasChild("dayPersRatio")){
+                        perRatio = Integer.parseInt(snapshot.child("dayPersRatio").getValue().toString());
+                    }else {
+                        perRatio = 0;
+                    }
+
+                    float othRatio;
+                    if (snapshot.hasChild("dayOtherRatio")){
+                        othRatio = Integer.parseInt(snapshot.child("dayOtherRatio").getValue().toString());
+                    }else {
+                        othRatio = 0;
+                    }
+
+                    float monthTotalSpentAmountRatio;
+                    if (snapshot.hasChild("dailyBudget")){
+                        monthTotalSpentAmountRatio = Integer.parseInt(snapshot.child("dailyBudget").getValue().toString());
+                    }else {
+                        monthTotalSpentAmountRatio = 0;
+                    }
+
+
+                    float monthPercent = (monthTotalSpentAmount / monthTotalSpentAmountRatio) * 100;
+                    binding.monthRatioSpending.setText(monthPercent + "% used of " + monthTotalSpentAmountRatio + ". Status: " );
+                    if (monthPercent < 50){
+                        binding.monthRatioSpendingImage.setImageResource(R.drawable.green);
+                    } else if (monthPercent >= 50 && monthPercent < 100){
+                        binding.monthRatioSpendingImage.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.monthRatioSpendingImage.setImageResource(R.drawable.red);
+                    }
+
+                    float transportPercent = (traTotal / traRatio) * 100;
+                    binding.progressRatioTransport.setText(transportPercent + "% used of " + traRatio + ". Status: " );
+                    if (transportPercent < 50){
+                        binding.transportStatus.setImageResource(R.drawable.green);
+                    } else if (transportPercent >= 50 && transportPercent < 100){
+                        binding.transportStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.transportStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float foodPercent = (foodTotal / foodRatio) * 100;
+                    binding.progressRatioFood.setText(foodPercent + "% used of " + foodRatio + ". Status: " );
+                    if (foodPercent < 50){
+                        binding.foodStatus.setImageResource(R.drawable.green);
+                    } else if (foodPercent >= 50 && foodPercent < 100){
+                        binding.foodStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.foodStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float housePercent = (houseTotal / houseRatio) * 100;
+                    binding.progressRatioHouse.setText(housePercent + "% used of " + houseRatio + ". Status: " );
+                    if (housePercent < 50){
+                        binding.houseStatus.setImageResource(R.drawable.green);
+                    } else if (housePercent >= 50 && housePercent < 100){
+                        binding.houseStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.houseStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float entPercent = (entTotal / entRatio) * 100;
+                    binding.progressRatioEntertainment.setText(entPercent + "% used of " + entRatio + ". Status: " );
+                    if (entPercent < 50){
+                        binding.entertainmentStatus.setImageResource(R.drawable.green);
+                    } else if (entPercent >= 50 && entPercent < 100){
+                        binding.entertainmentStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.entertainmentStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float educPercent = (eduTotal / eduRatio) * 100;
+                    binding.progressRatioEducation.setText(educPercent + "% used of " + eduRatio + ". Status: " );
+                    if (educPercent < 50){
+                        binding.educationStatus.setImageResource(R.drawable.green);
+                    } else if (educPercent >= 50 && educPercent < 100){
+                        binding.educationStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.educationStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float chaPercent = (chaTotal / charRatio) * 100;
+                    binding.progressRatioCharity.setText(chaPercent + "% used of " + charRatio + ". Status: " );
+                    if (chaPercent < 50){
+                        binding.charityStatus.setImageResource(R.drawable.green);
+                    } else if (chaPercent >= 50 && chaPercent < 100){
+                        binding.charityStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.charityStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float appPercent = (appTotal / apparelRatio) * 100;
+                    binding.progressRatioApparel.setText(appPercent + "% used of " + apparelRatio + ". Status: " );
+                    if (appPercent < 50){
+                        binding.apparelStatus.setImageResource(R.drawable.green);
+                    } else if (appPercent >= 50 && appPercent < 100){
+                        binding.apparelStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.apparelStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float healPercent = (heaTotal / heaRatio) * 100;
+                    binding.progressRatioHealth.setText(healPercent + "% used of " + heaRatio + ". Status: " );
+                    if (healPercent < 50){
+                        binding.healthStatus.setImageResource(R.drawable.green);
+                    } else if (healPercent >= 50 && healPercent < 100){
+                        binding.healthStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.healthStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float persPercent = (perTotal / perRatio) * 100;
+                    binding.progressRatioPersonal.setText(persPercent + "% used of " + perRatio + ". Status: " );
+                    if (persPercent < 50){
+                        binding.personalStatus.setImageResource(R.drawable.green);
+                    } else if (persPercent >= 50 && persPercent < 100){
+                        binding.personalStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.personalStatus.setImageResource(R.drawable.red);
+                    }
+
+                    float otherPercent = (othTotal / othRatio) * 100;
+                    binding.progressRatioOther.setText(otherPercent + "% used of " + othRatio + ". Status: " );
+                    if (otherPercent < 50){
+                        binding.otherStatus.setImageResource(R.drawable.green);
+                    } else if (otherPercent >= 50 && otherPercent < 100){
+                        binding.otherStatus.setImageResource(R.drawable.brown);
+                    } else {
+                        binding.otherStatus.setImageResource(R.drawable.red);
+                    }
 
 
                 }else {
-                    Toast.makeText(DailyAnalyticsActivity.this, "Child does not exist", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(DailyAnalyticsActivity.this, "SetStatusAndImageResource error", Toast.LENGTH_SHORT).show();
                 }
 
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(DailyAnalyticsActivity.this, "Child does not exist", Toast.LENGTH_SHORT).show();
+                Toast.makeText(DailyAnalyticsActivity.this, "SetStatusAndImageResource error", Toast.LENGTH_SHORT).show();
             }
         });
 
-    } //TODO
+    }
 
 
 
